092034e962aed2bade194fb632372c6ff0e21958
André Paris <apoliveira@uolinc.com>
2014-12-16 09:12:22 -0200

    Indroduzido cabeçalhos de lincensa nas classes

--- src/main/java/br/com/uol/runas/controller/JUnitController.java
+++ src/main/java/br/com/uol/runas/controller/JUnitController.java
@@ -1,3 +1,18 @@
+/*
+ *    Copyright 2013-2014 UOL - Universo Online Team
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
 package br.com.uol.runas.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,17 +24,17 @@ import br.com.uol.runas.service.JUnitService;
 
 @RestController
 public class JUnitController {
-	
+
 	@Autowired
 	private JUnitService jUnitService;
-	
+
 	@RequestMapping("/junit")
 	public String run(@RequestParam("path") String path, @RequestParam("suits") String[] suits) throws Exception {
-		
+
 		jUnitService.scan(path, suits);
-		
-		return "Oi";		
-	} 
-	
-	
+
+		return "Oi";
+	}
+
+
 }
--- src/main/java/br/com/uol/runas/service/JUnitService.java
+++ src/main/java/br/com/uol/runas/service/JUnitService.java
@@ -1,3 +1,18 @@
+/*
+ *    Copyright 2013-2014 UOL - Universo Online Team
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
 package br.com.uol.runas.service;
 
 import java.io.File;
@@ -15,24 +30,24 @@ import br.com.uol.runas.factory.ThreadFactoryImpl;
 
 @Service
 public class JUnitService {
-	
+
 	public Result scan(String path, final String[] suits) throws Exception {
-		
+
 		URL[] url = new URL[] {new File(path).toURI().toURL()};
 		final ClassLoader classLoader = URLClassLoader.newInstance(url);
-		
-		ThreadFactory threadFactory = new ThreadFactoryImpl(classLoader);			
-		
+
+		ThreadFactory threadFactory = new ThreadFactoryImpl(classLoader);
+
 		ExecutorService service = Executors.newSingleThreadExecutor(threadFactory);
-		
+
 		Class<?>[] classes = new Class[suits.length];
-		
+
 		for (int i = 0; i < suits.length; i++) {
 			classes[i] = classLoader.loadClass(suits[i]);
 		}
-		
+
 		Result result = service.submit(new JUnitCallable(classes)).get();
-		
+
 		return result;
 	}
 }
